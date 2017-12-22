e4009b7dc45ec10871a27df8fed730a9b3267a1a
Dwight <carter91jr@gmail.com>
2017-12-22 14:58:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+$(function () {
+    var pokemonName;
+    var defaultPokemon = '1';
+    var defaultPokemonData;
+
+    var initFunc = function () {
+        defaultPokemonData = $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
+            method: "GET",
+        });
+        defaultPokemonData.done(function (data) {
+            defaultPokemonData = data
+            $('.loading-container').removeClass('active')
+            $('.pokedex h3').text(data.name.toUpperCase());
+            $('.poke-img img').attr('src', data.sprites.front_default)
+            console.log(data)
+        });
+        defaultPokemonData.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error);
+        });
+    }
+
+    initFunc()
+
+
+    $(".btn").on("click", function () {
+        pokemonName = $('.pokedex input[type="text"]').val()
+        var request = $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
+            method: "GET",
+        });
+        request.done(function (data) {
+            $('.pokedex h3').text(data.name.toUpperCase());
+            $('.poke-img img').attr('src', data.sprites.front_default)
+            console.log(data)
+        });
+        request.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error);
+        });
+    })
+});
